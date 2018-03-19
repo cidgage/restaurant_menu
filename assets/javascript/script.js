017231f25d163438f764e0c3f0957fb8f55eda33
Na-Nate Feeeney <nanate614@gmail.com>
2018-03-18 22:01:28 -0400

    0318-10:01

---
+++ assets/javascript/script.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB0lJ0692c46AdStIP6c8MgSYZEF7P7e-Y",
+    authDomain: "project-1-6583a.firebaseapp.com",
+    databaseURL: "https://project-1-6583a.firebaseio.com",
+    projectId: "project-1-6583a",
+    storageBucket: "project-1-6583a.appspot.com",
+    messagingSenderId: "916964143148"
+  };
+  firebase.initializeApp(config);
